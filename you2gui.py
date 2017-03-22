714c88ea9a53655440daa9af5cd999b1fedddb40
doyousketch2 <doyousketch2@yahoo.com>
2017-03-22 18:24:04 -0400

    Update you2gui.py

--- you2gui.py
+++ you2gui.py
@@ -45,25 +45,32 @@ y2  = 'youtube-dl'
 ##  script  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 clipboard  = clip .paste()
+if not clipboard .startswith('http'):  ##  only autofill URL box if clipboard has a URL
+  clipboard  = ''
 
 ##  enterbox(msg='Enter something.', title=' ', default='', strip=True, image=None, root=None)
 URL  = eg .enterbox('URL?',  title,  clipboard)
 if URL is None:   sys .exit()   ##  exit if Cancel pressed
 if len(URL) < 1:   sys .exit()   ##  exit if no URL typed in, yet pressed OK
 
+getvidname  = [y2,  '-e',  URL]
+
+info  = sp .Popen(gathering,  stdout = sp .PIPE,  stderr = sp .PIPE)
+vidname, viderr  = info .communicate()
+
+if len(viderr) > 0:   print(viderr);   sys .exit()   ##  exit if there's an error
+
 gathering  = [y2,  '-F',  URL]
 
 info  = sp .Popen(gathering,  stdout = sp .PIPE,  stderr = sp .PIPE)
 out, err  = info .communicate()
 
-if len(err) > 0:   ##  exit if there's an error
-  print(err)
-  sys .exit()
+if len(err) > 0:   print(err);   sys .exit()   ##  exit if there's an error
 
 choices  = out .split('\n')[6:-1]   ##  skip misc text and blank entries
 
 ##  choicebox(msg='Pick something.', title=' ', choices=())
-choice  = eg .choicebox('Format?',  title,  choices)
+choice  = eg .choicebox(vidname + '\n\nFormat?',  title,  choices)
 if choice is None:   sys .exit()   ##  exit if dialog box closed
 
 code  = choice .split(' ')[0]   ##  keep number code, discard text
